185e90793d62bf88e79f9768db67275479e811fe
programc45 <caiof0251@gmail.com>
2025-02-23 15:55:56 +0000

    SUPER TRUNFO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,42 @@
 #include <stdio.h>
-
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+  
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("desafio nivel novato!\n");
+
+    int população;
+    int numerodepontosturisticos; 
+    float pib;
+    double area;
+    char A;
+    char A01[20];
+    char nome[50];
+
+    printf("qual o numero da população: \n");
+    scanf("%d" , &população);
+
+    printf("qual o numero de pontos turisticos: \n");
+    scanf("%d" , &numerodepontosturisticos);
+
+    printf("qual numero do pib: \n");
+    scanf("%f", &pib);
+
+    printf("qual numero do area: \n");
+    scanf("%2.f" , &area);
+
+    printf("qual estado da carta: \n");
+    scanf("%s" , &A);
+
+    printf("qual codigo da carta: \n");
+    scanf("%s" , &A01);
+
+    printf("qual nome da cidade: \n");
+    scanf("%s" , &nome );
+
+    printf("Numero da populção: %d - Numerosdepontosturisticos: %d\n" , população, numerodepontosturisticos);
+    printf("Numero do pib: %f - Area: %2.f\n" , pib , area);
+    printf("Estado da carta: %s - Carta: %s\n - Nome da cidade: %s \n" , A , A01 , nome);
+
 
     return 0;
 }
